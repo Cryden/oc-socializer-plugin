f24dc2bb6f8005d5ede98718f28befb40676578b
CRYDEsigN <Cryden_da@mail.ru>
2018-10-01 17:09:24 +0300

    Add Vk php sdk

--- formwidgets/Crossposting.php
+++ formwidgets/Crossposting.php
@@ -4,15 +4,20 @@ use Backend\Classes\FormWidgetBase;
 
 class CrossPosting extends FormWidgetBase
 {
-    /**
-     * @var string A unique alias to identify this widget.
-     */
-    protected $defaultAlias = 'crossposting';
 
-    public function render() 
-    {
-      return $this->makePartial('crossposting');
-    }
+  /**
+   * @var string A unique alias to identify this widget.
+   */
+  protected $defaultAlias = 'crossposting';
+
+  // Настройки полей OpenGraph
+  
+  // public $
+
+  public function render() 
+  {
+    return $this->makePartial('crossposting');
+  }
 }
 
 ?>
