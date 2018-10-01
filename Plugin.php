59188200272007a8554b3912af6ca9202a09840e
CRYDEsigN <Cryden_da@mail.ru>
2018-10-01 09:51:58 +0300

    plugin init

---
+++ Plugin.php
@@ -0,0 +1,18 @@
+<?php namespace Crydesign\Socializer;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+  public function register()
+  {
+  }
+
+  public function registerComponents()
+  {
+  }
+
+  public function registerSettings()
+  {
+  }
+}
---
+++ lang/ru/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Socializer',
+        'description' => 'Добавь социальные сети на свой сайт'
+    ]
+];
