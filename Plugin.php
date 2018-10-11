f056651faffb4a9b2caf21847c205463f1a24251
CRYDEsigN <Cryden_da@mail.ru>
2018-10-11 10:22:13 +0300

    clear

--- Plugin.php
+++ Plugin.php
@@ -108,7 +108,7 @@ class Plugin extends PluginBase
 
             }
 
-            addTest($controlLibrary);
+            // addTest($controlLibrary);
             addCrossposting($controlLibrary);
 
         });
