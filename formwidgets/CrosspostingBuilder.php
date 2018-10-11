b71f5512e5c15ed716b4b45c4237e06283739c61
CRYDEsigN <Cryden_da@mail.ru>
2018-10-11 09:32:06 +0300

    fix

--- formwidgets/CrosspostingBuilder.php
+++ formwidgets/CrosspostingBuilder.php
@@ -1,31 +1,29 @@
 <?php namespace Crydesign\Socializer\FormWidgets;
 
-use RainLab\Builder\Widgets\DefaultControlDesignTimeProvider;
 use Backend\Classes\WidgetBase;
+use RainLab\Builder\Widgets\DefaultControlDesignTimeProvider;
 
 class CrosspostingBuilder extends DefaultControlDesignTimeProvider
 {
-  protected $defaultControlsTypes = [
-    'crossposting',
-  ];
+    protected $defaultControlsTypes = [
+        'crossposting',
+    ];
 
-  public function renderControlBody($type, $properties, $formBuilder)
-  {
-     if (!in_array($type, $this->defaultControlsTypes)) {
-         return $this->renderUnknownControl($type, $properties);
-     }
+    public function renderControlBody($type, $properties, $formBuilder)
+    {
+        if (!in_array($type, $this->defaultControlsTypes)) {
+            return $this->renderUnknownControl($type, $properties);
+        }
 
-     //foreach ($this->defaultControlsTypes as $value) {
-     // WidgetBase::addViewPath(plugins_path()."/crydesign/socializer/formwidgets/".$value."/partials");
-    // }
+        // foreach ($this->defaultControlsTypes as $value) {
+        //     WidgetBase::addViewPath(plugins_path() . "/crydesign/socializer/formwidgets/" . $value . "/partials");
+        // }
 
-     return $this->makePartial($type, 
-      [
-        'properties'=>$properties,
-        'formBuilder' => $formBuilder
-      ]
-    );
-  }
+        return $this->makePartial($type,
+            [
+                'properties' => $properties,
+                'formBuilder' => $formBuilder,
+            ]
+        );
+    }
 }
-
-?>
